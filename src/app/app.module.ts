dde3e791d4ac7ec8f165445a5c86e61f4b6090a6
Tachi Dito <tachidito_7inc@hotmail.com>
2022-07-15 22:48:57 -0500

    nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 
 @NgModule({
@@ -11,6 +13,8 @@ import { HomeComponent } from './components/home/home.component';
     AppComponent,
     NavbarComponent,
     HomeComponent,
+    AboutComponent,
+    HeroesComponent,
   ],
   imports: [
     BrowserModule
